57adaf71e5ac9cff6ec4065e3248445384871d92
Stefan Marr <git@stefan-marr.de>
2016-09-17 12:21:58 +0200

    Use version in name for uploads
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- upload.sh
+++ upload.sh
@@ -4,12 +4,12 @@ BINTRAY_API_KEY=c0aa8fadc8f18afaa833761be21bea0f570bc0b0
 curl -H "X-Bintray-Override: 1" -H "X-Bintray-Publish: 1" \
   -T somns-deps-source.jar \
   -usmarr:$BINTRAY_API_KEY \
-  https://api.bintray.com/content/smarr/SOM/SOMns-deps/$VERSION/somns-deps-source.jar
+  https://api.bintray.com/content/smarr/SOM/SOMns-deps/$VERSION/somns-deps-source-$VERSION.jar
 curl -H "X-Bintray-Override: 1" -H "X-Bintray-Publish: 1" \
   -T somns-deps.jar \
   -usmarr:$BINTRAY_API_KEY \
-  https://api.bintray.com/content/smarr/SOM/SOMns-deps/$VERSION/somns-deps.jar
+  https://api.bintray.com/content/smarr/SOM/SOMns-deps/$VERSION/somns-deps-$VERSION.jar
 curl -H "X-Bintray-Override: 1" -H "X-Bintray-Publish: 1" \
   -T somns-deps-dev.jar \
   -usmarr:$BINTRAY_API_KEY \
-  https://api.bintray.com/content/smarr/SOM/SOMns-deps/$VERSION/somns-deps-dev.jar
+  https://api.bintray.com/content/smarr/SOM/SOMns-deps/$VERSION/somns-deps-dev-$VERSION.jar
