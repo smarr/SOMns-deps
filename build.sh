5b07163970739ed4f5f2d1f733a80a65d1c123ae
Stefan Marr <git@stefan-marr.de>
2016-10-04 16:58:29 +0200

    Replace minimal-json with GSON
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 set -e
 (cd websocket         && ant jar source-jar)
 (cd coveralls-truffle && ant jar source-jar)
-(cd minimal-json      && ant jar source-jar)
 
 mkdir -p build
 mkdir -p build-source
@@ -11,19 +10,19 @@ mkdir -p downloads
 
 curl -o downloads/jline.jar https://repo1.maven.org/maven2/jline/jline/2.11/jline-2.11.jar
 curl -o downloads/junit.jar https://repo1.maven.org/maven2/junit/junit/4.12/junit-4.12.jar
+curl -o downloads/gson.jar  https://repo1.maven.org/maven2/com/google/code/gson/gson/2.7/gson-2.7.jar
 curl -o downloads/hamcrest-core.jar https://repo1.maven.org/maven2/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar
 
 cd build
 jar -xf ../websocket/dist/websocket.jar
 jar -xf ../coveralls-truffle/coveralls-truffle.jar
-jar -xf ../minimal-json/build/minimal-json.jar
+jar -xf ../downloads/gson.jar
 jar -xf ../downloads/jline.jar
 cd ..
 
 cd build-source
 jar -xf ../websocket/dist/websocket-source.jar
 jar -xf ../coveralls-truffle/coveralls-truffle-source.jar
-jar -xf ../minimal-json/build/minimal-json-source.jar
 cd ..
 
 cd build-dev
