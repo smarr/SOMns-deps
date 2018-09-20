c43956ea63b492e056fe1b30533030b022961ed2
Stefan Marr <git@stefan-marr.de>
2018-09-20 09:01:29 +0100

    Remove JLine
    Should not be needed for Truffle any longer.
    SOMns wasn’t using it directly so far either.
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ mkdir -p build-source
 mkdir -p build-dev
 mkdir -p downloads
 
-curl -o downloads/jline.jar https://repo1.maven.org/maven2/jline/jline/2.11/jline-2.11.jar
 curl -o downloads/junit.jar https://repo1.maven.org/maven2/junit/junit/4.12/junit-4.12.jar
 curl -o downloads/gson.jar  https://repo1.maven.org/maven2/com/google/code/gson/gson/2.7/gson-2.7.jar
 curl -o downloads/hamcrest-core.jar https://repo1.maven.org/maven2/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar
@@ -17,7 +16,6 @@ cd build
 jar -xf ../websocket/dist/websocket.jar
 jar -xf ../coveralls-truffle/coveralls-truffle.jar
 jar -xf ../downloads/gson.jar
-jar -xf ../downloads/jline.jar
 cd ..
 
 cd build-source
